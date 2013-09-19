b7ee9aa7520aab82056b6169beaae0547652c56d
asamarco <marcoasa90@gmail.com>
2013-09-19 19:07:23 +0200

    set configuration bits

--- PierinPIC18.X/configuration_bits.c
+++ PierinPIC18.X/configuration_bits.c
@@ -1,50 +1,3 @@
-/******************************************************************************/
-/* Files to Include                                                           */
-/******************************************************************************/
-
-#if defined(__XC)
-    #include <xc.h>         /* XC8 General Include File */
-#elif defined(HI_TECH_C)
-    #include <htc.h>        /* HiTech General Include File */
-#elif defined(__18CXX)
-    #include <p18cxxx.h>    /* C18 General Include File */
-#endif
-
-/******************************************************************************/
-/* Configuration Bits                                                         */
-/*                                                                            */
-/* Refer to 'HI-TECH PICC and PICC18 Toolchains > PICC18 Configuration        */
-/* Settings' under Help > Contents in MPLAB X IDE for available PIC18         */
-/* Configuration Bit Settings for the correct macros when using the C18       */
-/* compiler.  When using the Hi-Tech PICC18 compiler, refer to the compiler   */
-/* manual.pdf in the compiler installation doc directory section on           */
-/* 'Configuration Fuses'.  The device header file in the HiTech PICC18        */
-/* compiler installation directory contains the available macros to be        */
-/* embedded.  The XC8 compiler contains documentation on the configuration    */
-/* bit macros within the compiler installation /docs folder in a file called  */
-/* pic18_chipinfo.html.                                                       */
-/*                                                                            */
-/* For additional information about what the hardware configurations mean in  */
-/* terms of device operation, refer to the device datasheet.                  */
-/*                                                                            */
-/* General C18/XC8 syntax for configuration macros:                           */
-/* #pragma config <Macro Name>=<Setting>, <Macro Name>=<Setting>, ...         */
-/*                                                                            */
-/* General HiTech PICC18 syntax:                                              */
-/* __CONFIG(n,x);                                                             */
-/*                                                                            */
-/* n is the config word number and x represents the anded macros from the     */
-/* device header file in the PICC18 compiler installation include directory.  */
-/*                                                                            */
-/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
-/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
-/* Configuration Bits, a user controllable configuration bits window is       */
-/* available to Generate Configuration Bits source code which the user can    */
-/* paste into this project.                                                   */
-/*                                                                            */
-/******************************************************************************/
-
-/* TODO Fill in your configuration bits here using the config generator.      */
 
 // PIC18F47J53 Configuration Bit Settings
 
@@ -54,22 +7,22 @@
 // Use project enums instead of #define for ON and OFF.
 
 // CONFIG1L
-#pragma config WDTEN = ON       // Watchdog Timer (Enabled)
-#pragma config PLLDIV = 1       // PLL Prescaler Selection (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config WDTEN = OFF      // Watchdog Timer (Disabled - Controlled by SWDTEN bit)
+#pragma config PLLDIV = 3       // PLL Prescaler Selection (Divide by 3 (12 MHz oscillator input))
 #pragma config CFGPLLEN = OFF   // PLL Enable Configuration Bit (PLL Disabled)
 #pragma config STVREN = ON      // Stack Overflow/Underflow Reset (Enabled)
-#pragma config XINST = ON       // Extended Instruction Set (Enabled)
+#pragma config XINST = OFF      // Extended Instruction Set (Disabled)
 
 // CONFIG1H
 #pragma config CPUDIV = OSC1    // CPU System Clock Postscaler (No CPU system clock divide)
 #pragma config CP0 = OFF        // Code Protect (Program memory is not code-protected)
 
 // CONFIG2L
-#pragma config OSC = ECPLL      // Oscillator (EC+PLL (CLKO-RA6), USB-EC+PLL)
+#pragma config OSC = HSPLL      // Oscillator (HS+PLL, USB-HS+PLL)
 #pragma config SOSCSEL = HIGH   // T1OSC/SOSC Power Selection Bits (High Power T1OSC/SOSC circuit selected)
 #pragma config CLKOEC = ON      // EC Clock Out Enable Bit  (CLKO output enabled on the RA6 pin)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor (Enabled)
-#pragma config IESO = ON        // Internal External Oscillator Switch Over Mode (Enabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor (Disabled)
+#pragma config IESO = OFF       // Internal External Oscillator Switch Over Mode (Disabled)
 
 // CONFIG2H
 #pragma config WDTPS = 32768    // Watchdog Postscaler (1:32768)
@@ -77,22 +30,22 @@
 // CONFIG3L
 #pragma config DSWDTOSC = INTOSCREF// DSWDT Clock Select (DSWDT uses INTRC)
 #pragma config RTCOSC = T1OSCREF// RTCC Clock Select (RTCC uses T1OSC/T1CKI)
-#pragma config DSBOREN = ON     // Deep Sleep BOR (Enabled)
-#pragma config DSWDTEN = ON     // Deep Sleep Watchdog Timer (Enabled)
-#pragma config DSWDTPS = G2     // Deep Sleep Watchdog Postscaler (1:2,147,483,648 (25.7 days))
+#pragma config DSBOREN = OFF    // Deep Sleep BOR (Disabled)
+#pragma config DSWDTEN = OFF    // Deep Sleep Watchdog Timer (Disabled)
+#pragma config DSWDTPS = 8192   // Deep Sleep Watchdog Postscaler (1:8,192 (8.5 seconds))
 
 // CONFIG3H
-#pragma config IOL1WAY = ON     // IOLOCK One-Way Set Enable bit (The IOLOCK bit (PPSCON<0>) can be set once)
+#pragma config IOL1WAY = OFF    // IOLOCK One-Way Set Enable bit (The IOLOCK bit (PPSCON<0>) can be set and cleared as needed)
 #pragma config ADCSEL = BIT10   // ADC 10 or 12 Bit Select (10 - Bit ADC Enabled)
 #pragma config MSSP7B_EN = MSK7 // MSSP address masking (7 Bit address masking mode)
 
 // CONFIG4L
-#pragma config WPFP = PAGE_127  // Write/Erase Protect Page Start/End Location (Write Protect Program Flash Page 127)
+#pragma config WPFP = PAGE_1    // Write/Erase Protect Page Start/End Location (Write Protect Program Flash Page 1)
 #pragma config WPCFG = OFF      // Write/Erase Protect Configuration Region  (Configuration Words page not erase/write-protected)
 
 // CONFIG4H
 #pragma config WPDIS = OFF      // Write Protect Disable bit (WPFP<6:0>/WPEND region ignored)
-#pragma config WPEND = PAGE_WPFP// Write/Erase Protect Region Select bit (valid when WPDIS = 0) (Pages WPFP<6:0> through Configuration Words erase/write protected)
+#pragma config WPEND = PAGE_0   // Write/Erase Protect Region Select bit (valid when WPDIS = 0) (Pages 0 through WPFP<6:0> erase/write protected)
 #pragma config LS48MHZ = SYS48X8// Low Speed USB mode with 48 MHz system clock bit (System clock at 48 MHz USB CLKEN divide-by is set to 8)
 
 
